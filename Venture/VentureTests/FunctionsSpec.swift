06580591359eaff8e63e41f261d0ec768d1340ac
Nico Hämäläinen <nico@sizeof.io>
2015-04-15 19:57:45 +0300

    Some Tests, Requirement "Header" to public

---
+++ Venture/VentureTests/FunctionsSpec.swift
@@ -0,0 +1,26 @@
+//
+//  FunctionsSpec.swift
+//  Venture
+//
+//  Created by Nico Hämäläinen on 15/04/15.
+//  Copyright (c) 2015 sizeof.io. All rights reserved.
+//
+import Quick
+import Nimble
+import Venture
+
+class FunctionsSpec: QuickSpec {
+    override func spec() {
+
+        describe("The `delay` function") {
+            it("should delay the execution by a given time") {
+                var value = false
+                delay(0.5) {
+                    value = true
+                }
+                expect(value).toEventually(beTrue())
+            }
+        }
+        
+    }
+}
